e5def2ebac3d40e1302cac1a93f5888a648fe75d
madzhunova <madzhunova_a@auca.kg>
2021-12-16 15:05:15 +0600

    Animation

--- app/src/main/java/kg/test/madzhunova_anelya_final/MainActivity.kt
+++ app/src/main/java/kg/test/madzhunova_anelya_final/MainActivity.kt
@@ -1,5 +1,6 @@
 package kg.test.madzhunova_anelya_final
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -7,12 +8,12 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.`product.xml`)
+        setContentView(R.layout.`product`)
 
         val textView = findViewById<TextView>(R.id.textView)
 
         textView.setOnClickListener{
-            startActivity(Int(this,MainActivity::class.java))
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 }
