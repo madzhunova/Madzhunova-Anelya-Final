7a0a969cad63a775a973f460ae0845a1186d46de
madzhunova <madzhunova_a@auca.kg>
2021-12-16 14:52:17 +0600

    1 frame & 2 edited

--- app/src/main/java/kg/test/madzhunova_anelya_final/MainActivity.kt
+++ app/src/main/java/kg/test/madzhunova_anelya_final/MainActivity.kt
@@ -2,10 +2,17 @@ package kg.test.madzhunova_anelya_final
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.`product.xml`)
+
+        val textView = findViewById<TextView>(R.id.textView)
+
+        textView.setOnClickListener{
+            startActivity(Int(this,MainActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/kg/test/madzhunova_anelya_final/Product.java
@@ -0,0 +1,6 @@
+package kg.test.madzhunova_anelya_final;
+
+import android.app.Activity;
+
+public class Product extends Activity {
+}
